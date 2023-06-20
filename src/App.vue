82996f28d3f742a757c06fb76b5ca8a0002dc2bf
Fatima <fawicoma@gmail.com>
2023-06-19 22:04:51 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    Day
+    DD
+
+    Month
+    MM
+
+    Year
+    YYYY
+
+    -- years
+    -- months
+    -- days
+    
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
